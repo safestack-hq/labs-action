78776a8661ce5cf99762fb4ccb6f7549a872498d
cfrichot <christian@safestack.io>
2022-06-04 09:13:07 +0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM aquasec/trivy:0.27.1 AS trivy-img
+
+FROM ghcr.io/safestack-hq/labs-validator:latest AS labs-validator
+RUN apk update && apk add --update bash
+
+COPY entrypoint.sh /entrypoint.sh
+COPY --from=trivy-img /usr/local/bin/trivy /bin/trivy
+
+USER 1000
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -e
+set -o pipefail
+echo ">>> Starting"
+echo ""
+
+bash -c "set -e; set -o pipefail; echo 'labs-validator version'; labs-validator --version"
+# bash -c "set -e; set -o pipefail; trivy --version"
+
+if [ $1 = "trivy" ]
+then
+  echo ">>> Trivy mode"
+  echo "Trivy version"
+  bash -c "set -e; set -o pipefail; trivy --version"
+  echo ">>> Trivy scanning table output"
+  bash -c "set -e; set -o pipefail; TRIVY_CACHE_DIR=.trivy trivy config -f table -s HIGH,CRITICAL ."
+  echo ">>> Trivy scanning json file output"
+  bash -c "set -e; set -o pipefail; TRIVY_CACHE_DIR=.trivy trivy config -f json -s HIGH,CRITICAL -o trivy.json ."
+
+  echo ">>> Validating results against labs.safestack.io"
+  bash -c "set -e; set -o pipefail; labs-validator trivy -json=trivy.json"
+fi
